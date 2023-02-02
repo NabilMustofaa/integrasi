14b9b977f25e51bd89a96eb7d3dce2fc3148caf3
NabilMustofaa <nabilmustofa6@gmail.com>
2023-02-02 16:05:27 +0700

    devextreme grid for stores and mapbox js

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -18,9 +18,10 @@
 @endsection
 
 @section('content')
-<h1 class=" text-3xl font-bold"> Store</h1>
 
-<div class=" flex">
+
+<div class=" flex justify-between">
+  <h1 class=" text-3xl font-bold"> Store</h1>
   <button class="flex justify-between bg-red-400 rounded-md h-8 hover:bg-red-300" id="survey">
     <div class=" h-12 border-r-2 border-r-white py-1 px-1">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 my-auto text-white" fill="white" viewBox="0 0 24 24" stroke="currentColor">
